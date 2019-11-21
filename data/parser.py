22626dde7b839501cfe0fce04c3b4b4ab7d54807
merkle-root <42051175+merkle-root@users.noreply.github.com>
2019-11-21 00:34:13 -0600

    year of the bug

---
+++ data/parser.py
@@ -0,0 +1,15 @@
+import csv
+
+with open("raw_regular.csv") as in_file:
+    with open("combined.csv", 'w') as out_file:
+        writer = csv.writer(out_file)
+        for row in csv.reader(in_file):
+            if row[0] != "":
+                writer.writerow(row)
+
+with open("raw_premium.csv") as in_file:
+    with open("combined.csv", 'a') as out_file:
+        writer = csv.writer(out_file)
+        for row in csv.reader(in_file):
+            if row[0] != "":
+                writer.writerow(row)
