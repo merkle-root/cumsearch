54ab32a129ecac4bda7619ce66e8e088c39458a6
merkle-root <42051175+merkle-root@users.noreply.github.com>
2019-11-24 21:01:59 -0600

    year of the bug

--- data/parser.py
+++ data/parser.py
@@ -1,5 +1,7 @@
 import csv
 
+print("Remember to delete the final 'END' entries before running this")
+
 with open("raw_regular.csv") as in_file:
     with open("combined.csv", 'w') as out_file:
         writer = csv.writer(out_file)
